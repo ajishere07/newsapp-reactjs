dcba6227abc3d26737820679c539867b333d364d
Anuj Yadav <anujpy.acct@gmail.com>
2021-12-02 22:03:06 +0530

    added pagination ✨

--- src/components/News.js
+++ src/components/News.js
@@ -6,18 +6,66 @@ export default class News extends Component {
   constructor() {
     super();
     this.state = {
+      page: 1,
       articles: [],
       loading: false,
+      totalArticles: 0,
     };
   }
   async componentDidMount() {
     let res = await axios.get(
-      "https://newsapi.org/v2/everything?q=bitcoin&apiKey=50455fa67dd042a08abb0b7af3cb514c"
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=50455fa67dd042a08abb0b7af3cb514c&page=${this.state.page}&pageSize=9`
     );
     console.log(res.data);
     let parsedData = await res.data;
-    this.setState({ articles: parsedData.articles });
+    this.setState({
+      articles: parsedData.articles,
+      totalArticles: parsedData.totalResults,
+    });
   }
+  // handlePrevPage = async () => {
+  //   await this.setState({
+  //     page: (this.state.page -= 1),
+  //   });
+  //   let res = await axios.get(
+  //     `https://newsapi.org/v2/top-headlines?country=in&apiKey=50455fa67dd042a08abb0b7af3cb514c&page=${this.state.page}`
+  //   );
+  //   // console.log(res.data);
+  //   let parsedData = await res.data;
+  //   this.setState({ articles: parsedData.articles });
+  // };
+  // handleNextPage = async () => {
+  //   await this.setState({
+  //     page: (this.state.page += 1),
+  //   });
+  //   let res = await axios.get(
+  //     `https://newsapi.org/v2/top-headlines?country=in&apiKey=50455fa67dd042a08abb0b7af3cb514c&page=${this.state.page}`
+  //   );
+  //   // console.log(res.data);
+  //   let parsedData = await res.data;
+  //   this.setState({ articles: parsedData.articles });
+  // };
+
+  handlePageRequest = async (a) => {
+    if (a === "next") {
+      if (this.state.page < Math.ceil(this.state.totalArticles / 9)) {
+        this.setState({
+          page: (this.state.page += 1),
+        });
+      }
+    } else if (a === "prev") {
+      this.setState({
+        page: (this.state.page -= 1),
+      });
+    }
+    let res = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=50455fa67dd042a08abb0b7af3cb514c&page=${this.state.page}&pageSize=9`
+    );
+    // console.log(res.data);
+    let parsedData = await res.data;
+    this.setState({ articles: parsedData.articles });
+  };
+
   render() {
     console.log("rendered");
     return (
@@ -27,13 +75,48 @@ export default class News extends Component {
           {this.state.articles.map((item) => (
             <div key={item.url} className="col-md-4">
               <NewsItem
-                title={item?.title.slice(0, 88)}
-                description={item?.description.slice(0, 85)}
-                url={item?.urlToImage}
-                newsUrl={item?.url}
+                title={item.title === null ? "null" : item.title.slice(0, 88)}
+                description={
+                  item.description === null
+                    ? "null"
+                    : item.description.slice(0, 85)
+                }
+                url={item.urlToImage === null ? "null" : item.urlToImage}
+                newsUrl={item.url === null ? "null" : item.url}
               />
             </div>
           ))}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            class="btn btn-dark"
+            onClick={() => {
+              this.handlePageRequest("prev");
+            }}
+          >
+            &larr; Previous
+          </button>
+          <p style={{ fontSize: "2rem" }}>{this.state.page}</p>
+          <button
+            disabled={
+              this.state.page === Math.ceil(this.state.totalArticles / 9)
+            }
+            type="button"
+            class="btn btn-dark"
+            onClick={() => {
+              this.handlePageRequest("next");
+            }}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
